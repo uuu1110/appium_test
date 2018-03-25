62634bdf501251ca93406ca46a7b2e1f7b1422a4
uuu1110 <236085528@qq.com>
2018-03-25 18:31:49 +0800

    fix bug

--- appiumTest.py
+++ appiumTest.py
@@ -13,9 +13,7 @@ caps["appActivity"] = ".view.WelcomeActivityAlias"
 
 driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
 time.sleep(5)
-el5 = driver.find_element_by_id("com.xueqiu.android:id/tv_login_by_phone_or_others")
+el5 = driver.find_element_by_xpath("//*[@text='港股']")
 el5.click()
-el6 = driver.find_element_by_id("com.xueqiu.android:id/register_phone_number")
-el6.send_keys("15107780353")
 time.sleep(10)
 driver.quit()
